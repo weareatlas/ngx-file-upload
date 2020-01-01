d361aad3f4f5a769c531d69f7005dfa482f69b6c
a-pradhan <ashish.pradhan@ats-global.com>
2020-01-01 16:04:01 +0000

    removed component prefix

--- projects/core-task/src/lib/component/task-file-upload/task-file-upload.component.ts
+++ projects/core-task/src/lib/component/task-file-upload/task-file-upload.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, Output, AfterViewInit, ViewChild, ElementRef, OnDestroy, EventEmitter } from '@angular/core';
 import { ConfigurationReaderService } from './service/cofiguration-reader.service';
 import { IConfiguration } from './model/configuration-type';
-import { Subject, fromEvent, Observable } from 'rxjs';
+import { Subject, fromEvent, Observable, Subscription } from 'rxjs';
 import {TaskFileUpload } from './task-file-upload';
 import { takeUntil } from 'rxjs/operators';
 import { ProgressState } from './model/progress-state';
@@ -10,7 +10,7 @@ import { TaskFileUploadService } from './service/task-file-upload.service';
 
 
 @Component({
-  selector: 'task-file-upload',
+  selector: 'file-upload',
   templateUrl: './task-file-upload.component.html',
   styleUrls: ['./task-file-upload.component.css']
 })
@@ -20,8 +20,7 @@ export class TaskFileUploadComponent implements OnInit, AfterViewInit, OnDestroy
    uploadStarted = false;
    @Input() configuration: IConfiguration;
    @Input() public initialMessage: string;
-   @Input() public taskTitle: string;
-   @Input() public taskSubTitle: string;
+   @Input() public title: string;
 
    @Output() uploaded: EventEmitter<TaskFileUpload> = new EventEmitter<TaskFileUpload>();
    @Output() deleted: EventEmitter<any> = new EventEmitter<any>();
@@ -40,7 +39,7 @@ export class TaskFileUploadComponent implements OnInit, AfterViewInit, OnDestroy
   //  public error$ = this.error.asObservable();
 
 
-   @ViewChild('taskFileInput' , {static: true}) taskFileInput: ElementRef;
+   @ViewChild('taskFileInput' , {static: true}) fileInput: ElementRef;
    @ViewChild('taskFileUploadParentContainer' , {static: false}) parentElement: ElementRef;
 
 
@@ -53,6 +52,14 @@ export class TaskFileUploadComponent implements OnInit, AfterViewInit, OnDestroy
    private unsubscribe$: Subject<void> = new Subject();
 
 
+
+   private clickSubscription: Subscription;
+   private dragOverSubscription: Subscription;
+   private dragLeaveSubscription: Subscription;
+   private dropSubscription: Subscription;
+   private inputChangeSubscription: Subscription;
+
+
   constructor(private configurationReaderService: ConfigurationReaderService, private taskFileUploadService: TaskFileUploadService) {
 
    }
@@ -60,8 +67,33 @@ export class TaskFileUploadComponent implements OnInit, AfterViewInit, OnDestroy
   ngOnInit() {
   }
 
-   delete(response: any) {
-    this.deleted.emit(JSON.parse(response.body));
+  delete(fileUpload: TaskFileUpload, event: any) {
+
+    event.stopPropagation();
+    this.taskFileUploadService.fileUploadMapping = this.taskFileUploadService.fileUploadMapping
+                                                      .filter(file =>  file !== fileUpload.FileInfo.name );
+
+
+    if (this.taskFileUploadService.fileUploadMapping  && this.taskFileUploadService.fileUploadMapping .length === 0) {
+      console.log(this.taskFileUploadService.fileUploadMapping.length);
+      this.getParentElement.className = this.configurationReaderService.config.dropZoneClass;
+      this.uploadStarted = false;
+
+      this.subscribeToFileSources();
+      // this.unsubscribe$.unsubscribe();
+      // this.ngOnDestroy();
+      // this.deRegisterFileSources();
+      // this.registerFileSources();
+      // this.subscribeToFileSources();
+    }
+
+    fileUpload.isDeleted  = true;
+
+    if (this.hasJsonStructure(fileUpload)) {
+      this.deleted.emit(JSON.parse(fileUpload.response.body));
+    } else {
+      this.deleted.emit(fileUpload.response.body);
+    }
 
   }
   private start() {
@@ -98,7 +130,7 @@ export class TaskFileUploadComponent implements OnInit, AfterViewInit, OnDestroy
 
       try {
           this.configurationReaderService.config = this.configuration;
-          this.configurationReaderService.Read().subscribe( this.parseConfigurationObserver());
+          this.configurationReaderService.read().subscribe( this.parseConfigurationObserver());
 
         } catch (error) {
           this.error.emit(error);
@@ -127,103 +159,106 @@ export class TaskFileUploadComponent implements OnInit, AfterViewInit, OnDestroy
     // Register file source
   public registerFileSources() {
 
-        this.clickEventObservable$ = fromEvent(this.getParentElement, 'click');
-        this.fileInputChangeEventObservable$ = fromEvent(this.getInputElement, 'change');
-        this.dragOverEventObservable$  = fromEvent(this.getParentElement, 'dragover');
-        this.dragLeaveEventObservable$  = fromEvent(this.getParentElement, 'dragleave');
-        this.dropEventObservable$ = fromEvent(this.getParentElement, 'drop');
+        this.clickEventObservable$ = fromEvent(this.getParentElement, 'click').pipe (takeUntil(this.unsubscribe$));
+        this.fileInputChangeEventObservable$ = fromEvent(this.getInputElement, 'change').pipe (takeUntil(this.unsubscribe$));
+        this.dragOverEventObservable$  = fromEvent(this.getParentElement, 'dragover').pipe (takeUntil(this.unsubscribe$));
+        this.dragLeaveEventObservable$  = fromEvent(this.getParentElement, 'dragleave').pipe (takeUntil(this.unsubscribe$));
+        this.dropEventObservable$ = fromEvent(this.getParentElement, 'drop').pipe (takeUntil(this.unsubscribe$));
+
   }
 
 
   public subscribeToFileSources() {
 
-    this.registerClickHandler();
-    this.registerFileInput();
+    this.clickSubscription = this.registerClickHandler();
+    this.inputChangeSubscription = this.registerFileInput();
     this.registerDropZone();
 
   }
 
 
-
-
-
     // register click handler on the parent DOM element
-  private registerClickHandler() {
+  private registerClickHandler(): Subscription {
 
-    this.clickEventObservable$
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe( (event) => { this.getInputElement.click(); });
+    return this.clickEventObservable$.subscribe( (event) => {
+       this.getInputElement.click();
+      });
   }
 
 
   // register file input component change event
-  private registerFileInput() {
+  private registerFileInput(): Subscription {
 
-    this.fileInputChangeEventObservable$
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe( (event) => {
+    return this.fileInputChangeEventObservable$.subscribe( (event) => {
 
-        this.taskFileUploadService.createFileUploadAsync(event.target.files).subscribe( (fileUpload: TaskFileUpload[]) => {
-          this.deRegisterFileSources();
-          this.fileUploadSubject.next(fileUpload);
+        if (event.target.files && event.target.files.length > 0) {
 
-      });
-
+            this.taskFileUploadService.createFileUploadAsync(event.target.files).subscribe( (fileUpload: TaskFileUpload[]) => {
+              this.deRegisterFileSources();
+              this.fileUploadSubject.next(fileUpload);
+          });
+        }
       });
   }
 
 
-  // Register drop zone and subscribe dragover,dragleave and drop events to get the dragged files
+  private registerDragOver(): Subscription {
+
+    return this.dragOverEventObservable$.subscribe( (event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          this.getParentElement.className =  this.configurationReaderService.config.dragOverClass;
+    });
+
+  }
+
+  private registerDragLeave(): Subscription {
+
+    return this.dragLeaveEventObservable$.subscribe( (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      this.getParentElement.className = this.configurationReaderService.config.dropZoneClass;
+    });
+
+  }
+
+  private registerDrop(): Subscription {
+
+    return this.dropEventObservable$.subscribe( (event) => {
+
+      event.preventDefault();
+      event.stopPropagation();
+      this.getParentElement.className = this.configurationReaderService.config.dropZoneClass;
+
+      // create the file upload object
+      this.taskFileUploadService.createFileUploadAsync (event.dataTransfer.files).subscribe( (fileUpload: TaskFileUpload[]) => {
+          this.deRegisterFileSources();
+          this.fileUploadSubject.next(fileUpload);
+      });
+
+  });
+
+  }
+
+
+// Register drop zone and subscribe dragover,dragleave and drop events to get the dragged files
   private registerDropZone() {
 
-
-    this.dragOverEventObservable$
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe( (event) => {
-
-        event.preventDefault();
-        event.stopPropagation();
-
-        this.getParentElement.className =  this.configurationReaderService.config.dragOverClass;
-      });
-
-    this.dragLeaveEventObservable$
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe( (event) => {
-          event.preventDefault();
-          event.stopPropagation();
-          this.getParentElement.className = this.configurationReaderService.config.dropZoneClass;
-      });
-
-    this.dropEventObservable$
-        .pipe(takeUntil(this.unsubscribe$))
-        .subscribe( (event) => {
-
-          event.preventDefault();
-          event.stopPropagation();
-          this.getParentElement.className = this.configurationReaderService.config.dropZoneClass;
-
-          // create the file upload object
-          this.taskFileUploadService.createFileUploadAsync (event.dataTransfer.files).subscribe( (fileUpload: TaskFileUpload[]) => {
-              this.deRegisterFileSources();
-              this.fileUploadSubject.next(fileUpload);
-          });
-
-      });
-
-    }
+    this.dragOverSubscription =  this.registerDragOver();
+    this.dragLeaveSubscription =  this.registerDragLeave();
+    this.dropSubscription =  this.registerDrop();
+  }
 
 
-
-
-      // Gets reference to file input element
-    private get getInputElement(): HTMLInputElement {
-        if (this.taskFileInput) {
-            return this.taskFileInput.nativeElement as HTMLInputElement;
+// Gets reference to file input element
+  private get getInputElement(): HTMLInputElement {
+        if (this.fileInput) {
+            return this.fileInput.nativeElement as HTMLInputElement;
         }
-    }
+  }
 
-    private get getParentElement(): HTMLElement {
+  // get reference to the input element
+  private get getParentElement(): HTMLElement {
       if (this.parentElement) {
           return this.parentElement.nativeElement as HTMLElement;
       }
@@ -231,14 +266,33 @@ export class TaskFileUploadComponent implements OnInit, AfterViewInit, OnDestroy
 
 
   ngOnDestroy(): void {
-      this.deRegisterFileSources();
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
 
   // DeRegister file sources
   private deRegisterFileSources(): void {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
+    this.clickSubscription.unsubscribe();
+    this.dragOverSubscription.unsubscribe();
+    this.dragLeaveSubscription.unsubscribe();
+    this.dropSubscription.unsubscribe();
+    this.inputChangeSubscription.unsubscribe();
   }
+
+
+  private hasJsonStructure(str) {
+    if (typeof str !== 'string') {
+      return false;
+    }
+
+    try {
+        const result = JSON.parse(str);
+        const type = Object.prototype.toString.call(result);
+        return type === '[object Object]' || type === '[object Array]';
+    } catch (err) {
+        return false;
+    }
+}
 
 }
