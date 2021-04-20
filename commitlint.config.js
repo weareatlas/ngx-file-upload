fc28fcb535e9e6e0a868fb14609f976826b0258a
ashish-data <ashish.pradhan@ats-global.com>
2021-04-20 10:04:50 +0100

    chore: updates commit lint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,16 @@
+const types = require('@commitlint/config-conventional');
 module.exports = {
-    extends: ['@commitlint/config-conventional']
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                ...types.rules['type-enum'][2],
+                ...[
+                    'release'
+                ]
+            ]
+        ]
+    }
 };
